14780f05c4c543d4efd09a520c08a63a2c10fa38
Rainey F <raineyfleming@protonmail.com>
2021-10-28 07:19:53 -0400

    add readme, title, better focus style and gh-pages

--- src/App.js
+++ src/App.js
@@ -6,18 +6,20 @@ function App() {
     const principal = useSelector((state) => state.principal.value);
     return (
         <div className="app-container">
-            <h1>Simple Interest Calculator</h1>
-            <Form label="Calculate Your Total Principal" />
-            <div aria-live="polite">
-                {isNaN(principal.calculation) ? (
-                    <p>
-                        Please ensure that all fields only contain numeric
-                        characters
-                    </p>
-                ) : (
-                    <p>Calculated Principal: ${principal.calculation}</p>
-                )}
-            </div>
+            <main>
+                <h1>Simple Interest Calculator</h1>
+                <Form label="Calculate Your Total Principal" />
+                <div aria-live="polite">
+                    {isNaN(principal.calculation) ? (
+                        <p>
+                            Please ensure that all fields only contain numeric
+                            characters
+                        </p>
+                    ) : (
+                        <p>Calculated Principal: ${principal.calculation}</p>
+                    )}
+                </div>
+            </main>
         </div>
     );
 }
